024354bd384f1b7d15e26ff50d24f8f09417266d
Steven Choi <steven.m.choi@gmail.com>
2018-06-04 03:40:02 -0700

    Create new GoogleStrategy

--- server/index.js
+++ server/index.js
@@ -1,15 +1,11 @@
-// Use common JS modules for Node.js (backend)
-// 'import' for React (frontend)
 const express = require( 'express' );
+const passport = require( 'passport' );
+const GoogleStrategy = require( 'passport-google-oauth20' ).Strategy;
+
 const app = express();
 
-app.get( '/', ( req, res ) => {
-	res.send( {
-		hi: 'there'
-	} );
-} );
+passport.use( new GoogleStrategy() );
 
 const PORT = process.env.PORT || 5000;
 
 app.listen( PORT );
-// http://localhost:5000/
