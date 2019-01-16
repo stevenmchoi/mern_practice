622480160730ef04219498376f4a83b5f2b5f9fa
Steven Choi <steven.m.choi@gmail.com>
2019-01-15 22:15:17 -0800

    Fix Heroku proxy issue for Google OAuth

--- server/services/passport.js
+++ server/services/passport.js
@@ -21,7 +21,9 @@ passport.use(
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
 			callbackURL: '/auth/google/callback',
+			proxy: true,
 		},
+
 		(accessToken, refreshToken, profile, done) => {
 			User.findOne({ googleId: profile.id }).then((existingUser) => {
 				if (existingUser) {
