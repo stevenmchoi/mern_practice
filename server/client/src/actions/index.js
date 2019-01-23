b19fcdb3f6e8c829cc13193807ae59ea96e2f135
Steven Choi <steven.m.choi@gmail.com>
2019-01-23 12:54:00 -0800

    Create fetchUser action

---
+++ server/client/src/actions/index.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import { FETCH_USER } from './types';
+
+export const fetchUser = () => (dispatch) => {
+	axios
+		.get('/api/current_user')
+		.then((user) => dispatch({ type: FETCH_USER, payload: user }));
+};
