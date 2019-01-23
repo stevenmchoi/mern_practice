7cc287101546721b7ea83f4260a47f3183252508
Steven Choi <steven.m.choi@gmail.com>
2019-01-23 10:08:36 -0800

    Use React Router for Landing route

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,8 +1,18 @@
 // Rendering layer control (React Router)
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-	return <div>Hi There!</div>;
+	return (
+		<Router>
+			<Route path="/" component={Landing} />
+		</Router>
+	);
 };
 
 export default App;
