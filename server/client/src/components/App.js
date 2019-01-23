87a688b8f3ca9d20ea9f8d7adea0526e07abdd5c
Steven Choi <steven.m.choi@gmail.com>
2019-01-23 10:25:33 -0800

    Add other Routes and Switch

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,6 +1,6 @@
 // Rendering layer control (React Router)
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const Header = () => <h2>Header</h2>;
 const Dashboard = () => <h2>Dashboard</h2>;
@@ -10,7 +10,14 @@ const Landing = () => <h2>Landing</h2>;
 const App = () => {
 	return (
 		<Router>
-			<Route path="/" component={Landing} />
+			<div>
+				<Header />
+				<Switch>
+					<Route exact path="/" component={Landing} />
+					<Route path="/surveys" component={Dashboard} />
+					<Route path="/surveys/new" component={SurveyNew} />
+				</Switch>
+			</div>
 		</Router>
 	);
 };
