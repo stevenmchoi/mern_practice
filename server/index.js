e257ebbbecf85dbd39635f7ddab97dab1073b871
Steven Choi <steven.m.choi@gmail.com>
2020-03-13 21:29:13 -0700

    Create prod keys

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 
-const keys = require('./config/dev');
+const keys = require('./config/keys');
 
 require('./models/User'); // Load models before passport uses them
 require('./services/passport');
