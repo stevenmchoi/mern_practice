aed8a6b802b29b2c2f05c4e05a655cd9572a9377
Steven Choi <steven.m.choi@gmail.com>
2020-03-16 14:59:41 -0700

    Style Header

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Header extends Component {
     render() {
         return (
-            <div>
-                Header
-            </div>
-        )
+            <nav>
+                <div className="nav-wrapper">
+                    <a href="#" className="left brand-logo">
+                        Emaily
+                    </a>
+                    <ul className="right">
+                        <li>
+                            <a>Login With Google</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        );
     }
 }
 
-export default Header
+export default Header;
