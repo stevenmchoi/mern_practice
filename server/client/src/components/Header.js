48fae911e5ad37c73b046baaf73f7fc474f3b29d
Steven Choi <steven.m.choi@gmail.com>
2020-03-16 17:20:24 -0700

    Change home page for logged in / logged out

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
     renderContent() {
@@ -8,11 +9,15 @@ class Header extends Component {
                 return;
             case false:
                 return (
-                    <li><a href="/auth/google">Login With Google</a></li>
+                    <li>
+                        <a href="/auth/google">Login With Google</a>
+                    </li>
                 );
             default:
                 return (
-                    <li><a href="/api/logout">Log Out</a></li>
+                    <li>
+                        <a href="/api/logout">Log Out</a>
+                    </li>
                 );
         }
     }
@@ -21,13 +26,14 @@ class Header extends Component {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    <a href="#" className="left brand-logo">
+                    <Link
+                        to={this.props.auth ? '/surveys' : '/'}
+                        className="left brand-logo"
+                    >
                         Emaily
-                    </a>
+                    </Link>
                     <ul className="right">
-                        <li>
-                            {this.renderContent()}
-                        </li>
+                        {this.renderContent()}
                     </ul>
                 </div>
             </nav>
