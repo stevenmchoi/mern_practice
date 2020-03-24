3326722ad8b5527cd1080d80ffdbd77416fc7e8f
Steven Choi <steven.m.choi@gmail.com>
2020-03-23 17:42:52 -0700

    Create Mailer service

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
@@ -9,20 +11,20 @@ module.exports = (app) => {
         // Receive body from frontend
         const { title, subject, body, recipients } = req.body;
 
-        const recipientArray = recipients.split(',').map((email) => ({
-            email: email.trim(),
-        }));
-
         const survey = new Survey({
             title,
             subject,
             body,
-            recipients: recipientArray,
+            recipients: recipients.split(',').map((email) => ({
+                email: email.trim(),
+            })),
             _user: req.user.id,
             dateSent: Date.now(),
         });
 
+        const mailer = new Mailer(survey, surveyTemplate(survey));
+
         // Send back to frontend
-        res.send(survey);
+        mailer.send();
     });
 };
