c512c9c423d70d6f7947dbf7817fb4a641a30635
Steven Choi <steven.m.choi@gmail.com>
2020-04-08 16:50:07 -0700

    Move review fields out of return statement

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -4,15 +4,17 @@ import { connect } from 'react-redux';
 import formFields from './formFields';
 
 const SurveyFormReview = ({ onCancel, formValues }) => {
+    const reviewFields = _.map(formFields, ({ label, name }) => (
+        <div key={name}>
+            <label>{label}</label>
+            <div>{formValues[name]}</div>
+        </div>
+    ));
+
     return (
         <div>
             <h5>Please confirm your entries</h5>
-            {_.map(formFields, ({ label, name }) => (
-                <div key={label}>
-                    <label>{label}</label>
-                    <div>{formValues[name]}</div>
-                </div>
-            ))}
+            {reviewFields()}
             <button className="yellow darken-3 btn-flat" onClick={onCancel}>
                 Back
             </button>
