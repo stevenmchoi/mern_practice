d1e2261494a6e030dab892e0c24bf09c9f23ba3b
Steven Choi <steven.m.choi@gmail.com>
2020-04-09 02:58:09 -0700

    Return FETCH_USER instead

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER, SUBMIT_SURVEY } from './types';
+import { FETCH_USER } from './types';
 
 export const fetchUser = () => async (dispatch) => {
     const res = await axios.get('/api/current_user');
@@ -11,9 +11,7 @@ export const handleToken = (token) => async (dispatch) => {
     dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const submitSurvey = (formValues) => async (dispatch) => {
-    console.log(formValues);
-
-    const res = await axios.post('/api/survey', formValues);
-    dispatch({ type: SUBMIT_SURVEY, payload: res.data });
+export const submitSurvey = (values) => async (dispatch) => {
+    const res = await axios.post('/api/survey', values);
+    dispatch({ type: FETCH_USER, payload: res.data });
 };
