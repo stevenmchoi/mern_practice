a86bd0a360c1faa2de9b4a603e14932b2c1da609
Steven Choi <steven.m.choi@gmail.com>
2020-04-09 03:20:13 -0700

    Enable redirect to dashboard after sending surveys

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -1,10 +1,11 @@
 import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import formFields from './formFields';
 import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
     const reviewFields = _.map(formFields, ({ label, name }) => (
         <div key={name}>
             <label>{label}</label>
@@ -24,7 +25,7 @@ const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
             </button>
             <button
                 className="green btn-flat right white-text"
-                onClick={() => submitSurvey(formValues)}
+                onClick={() => submitSurvey(formValues, history)}
             >
                 Send Survey
                 <i className="material-icons right">email</i>
@@ -41,4 +42,4 @@ function mapStateToProps({
     return { formValues: values };
 }
 
-export default connect(mapStateToProps, actions)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
