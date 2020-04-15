d7322a94a821052bdce8e702d9040b1215dcf59f
Steven Choi <steven.m.choi@gmail.com>
2020-04-15 16:26:24 -0700

    Fix mongoose DeprecationWarnings

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ require('./models/User'); // Load models before passport uses them
 require('./models/Survey');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
 
 const app = express();
 
